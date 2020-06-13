4e822c15759d23b8afe74810697d514f92556996
OB.MacBook <obmacbook@OBMacBooks-MacBook-Pro-7.local>
2020-06-13 12:00:28 -0500

    Python fundamentals labs

--- 01_python_fundamentals/01_02_seconds_years.py
+++ 01_python_fundamentals/01_02_seconds_years.py
@@ -4,3 +4,8 @@ Move the code you previously wrote to calculate how many seconds are in a year i
 Then execute it as a script to see the output printed to your console.
 
 '''
+seconds_in_hr = 3600
+hrs_in_day = 24
+days_in_yr = 365
+sec_in_yr = seconds_in_hr*hrs_in_day*days_in_yr
+print("There are {} seconds in a year".format(sec_in_yr))
--- 01_python_fundamentals/01_06_hello5.py
+++ 01_python_fundamentals/01_06_hello5.py
@@ -4,3 +4,4 @@ Write the necessary code to display "Hello World!" 5 times.
 
 
 '''
+print("Hello World!\n"*5)
